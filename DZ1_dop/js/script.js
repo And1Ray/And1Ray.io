b4125c4a8e4e7fdd3cf833d7b73d0b91ecbffb5f
Andrey Petukhov <me4life2009@rambler.ru>
2018-06-02 23:21:38 +0300

    Первая доп домашка

---
+++ DZ1_dop/js/script.js
@@ -0,0 +1,10 @@
+var num = 33721;
+
+/*num = 3 * 3 * 7 * 2 * 1;
+console.log(num);
+num = String(num ** 3);
+console.log(num);
+console.log(num[0] + num[1]);*/
+
+num = String((3 * 3 * 7 * 2 * 1) ** 3);
+alert(num[0] + num[1]);
