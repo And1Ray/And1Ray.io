087508aa89ce5cca7fd18e06e7f8730623b819cc
And1Ray <36099876+And1Ray@users.noreply.github.com>
2018-06-22 23:38:39 +0300

    ДЗ14

---
+++ DZ14/js/bundle/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+	let tab = require('../parts/tabs.js');
+	let modal = require('../parts/modal.js');
+	let form = require('../parts/form.js');
+	let slider = require('../parts/slider.js');
+	let calc = require('../parts/calc.js');
+	let timer = require('../parts/timer.js');
+
+	tab();
+	modal();
+	form();
+	slider();
+	calc();
+	timer();
+});
