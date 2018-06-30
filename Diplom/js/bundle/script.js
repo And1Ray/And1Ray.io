f782410f7996c5d01ae750916daada1fe30776c4
Andrey Petukhov <me4life2009@rambler.ru>
2018-07-01 00:29:45 +0300

    Дипломная работа, первые 2 модальных окна.

---
+++ Diplom/js/bundle/script.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+//Modal
+
+	let btnPopupEngineer = document.querySelector('.popup_engineer_btn'),
+			phoneLink = document.querySelector('.phone_link'),
+			modalPopupEngineer = document.querySelector('.popup_engineer'),
+			closeButton = document.getElementsByClassName('popup_close')[0],
+			closeButtonEngineer = document.getElementsByClassName('popup_close')[1],
+			modalPopup = document.querySelector('.popup');
+
+
+	function modalWindowOpenClose (elem, value, modal) {
+		elem.addEventListener('click', () => {
+			modal.style.display = value;
+		});
+
+		modal.addEventListener('click', function(event) {
+				let target = event.target;
+				if (target == this) {
+					modal.style.display = 'none';
+			}
+		});
+	}
+
+	modalWindowOpenClose (btnPopupEngineer, 'flex', modalPopupEngineer);
+	modalWindowOpenClose (closeButtonEngineer, 'none', modalPopupEngineer);
+
+	modalWindowOpenClose(phoneLink, 'flex', modalPopup);
+	modalWindowOpenClose (closeButton, 'none', modalPopup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
